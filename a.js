ca5b88c5fdd7a24a3480a972cc5ce13dc8e2e898
Franco Valencia <franco.avalencia@gmail.com>
2017-02-28 22:04:59 -0600

    Making code suck less

--- a.js
+++ a.js
@@ -19,10 +19,9 @@ var inputCount = 1; // Counter for input
 var crane;
 var state = new State();
 
-var visited = [];
+var visitedNodes = [];
 var queue = new Queue();
 var movements = new Stack();
-var prevStates = new Stack();
 var cost = 0;
 
 rl.on('line', function(line) {
@@ -53,55 +52,16 @@ function main(maxHeight, initialState, goalState) {
   var goal = state.parse(goalState);
   crane = new Crane(maxHeight);
 
-  // testHeap(init, goal);
-  // console.log(state.getNumberMisplacedBlocks(init, goal));
-
   if (!astar(init, goal)) {
     console.log('No solution found');
   } else {
     console.log(cost);
-    printMovements();
+    state.printMovements(movements);
   }
-  //
-  //
-}
-
-function testHeap(init, goal) {
-  var nextValidStates = crane.nextValidStates(init);
-  var heap = new BinaryHeap(function(x) {
-    return state.getNumberMisplacedStacks(x, goal);
-  });
-
-  for (var k = 0; k < nextValidStates.length; k++) {
-    heap.push(nextValidStates[k]);
-  }
-
-  console.log('Pop States --');
-
-  while (heap.size() > 0) {
-    var x = heap.pop();
-    console.log(x, state.getNumberMisplacedStacks(x, goal));
-  }
-}
-
-
-function printMovements() {
-  var arr = [];
-  var arr2 = [];
-
-  while (movements.length > 0) {
-    var n = movements.pop();
-    // n.print();
-    arr.push(n.action);
-  }
-
-  console.log(arr.join('; '));
 }
 
 function heuristic(node, goal, currentCost) {
-  // return state.getNumberMisplacedStacks(node, goal, currentCost);
   return state.getNumberMisplacedBlocks(node, goal);
-  // return 0;
 }
 
 
@@ -119,55 +79,30 @@ function astar(init, goal) {
 
   while (heap.size() > 0) {
 
-    // console.log('--Heap BEFORE Pop --');
-    // console.log(heap);
     node = heap.pop();
-    // console.log('--Heap After Pop --');
-    // console.log(heap);
-    // node.print();
 
     if (state.compare(node.state, goal)) {
       cost = node.cost;
       while (node.parent) {
-        // node.print();
         movements.push(node);
         node = node.parent;
       }
       return true;
     }
 
-    visited.push(node.state);
+    visitedNodes.push(node.state);
 
     var children = crane.nextValidStates(node.state);
-    // console.log('Posibilities:', children);
 
     for (var i = 0; i < children.length; i++) {
       child = children[i];
-      if (!visitedContains(child)) {
+      if (!state.visitedContains(visitedNodes, child)) {
         var currentCost = node.cost;
         var newNode = new Node(child, node);
         heap.push(newNode);
-        // console.log('--Heap after push --');
-        // console.log(heap);
       }
     }
   }
 }
 
-function visitedContains(stack) {
 
-  result = false;
-
-  for (j = 0; j < visited.length; j++) {
-    // console.log('--VisitedContains--');
-    // console.log('Checking:', stack);
-    // console.log('Against', visited[j]);
-    // console.log('Result:', state.compare(visited[j], stack));
-
-    if (state.compare(visited[j], stack)) {
-      result = true;
-    }
-  }
-
-  return result;
-}
--- a_star_icons.js
+++ a_star_icons.js
@@ -19,7 +19,7 @@ var inputCount = 1; // Counter for input
 var crane;
 var state = new State();
 
-var visited = [];
+var visitedNodes = [];
 var queue = new Queue();
 var movements = new Stack();
 var cost = 0;
@@ -52,44 +52,12 @@ function main(maxHeight, initialState, goalState) {
   var goal = state.parse(goalState);
   crane = new Crane(maxHeight);
 
-  // testHeap(init, goal);
-
   if (!astar(init, goal)) {
     console.log('No solution found');
   } else {
     console.log(cost);
-    printMovements();
+    state.printMovements(movements);
   }
-  //
-  //
-}
-
-function testHeap(init, goal) {
-  var nextValidStates = crane.nextValidStates(init);
-  var heap = new BinaryHeap(function(x) {
-    return state.getNumberMisplacedStacks(x, goal);
-  });
-
-  for (var k = 0; k < nextValidStates.length; k++) {
-    heap.push(nextValidStates[k]);
-  }
-
-  console.log('Pop States --');
-
-  while (heap.size() > 0) {
-    var x = heap.pop();
-    console.log(x, state.getNumberMisplacedStacks(x, goal));
-  }
-}
-
-
-function printMovements() {
-  var arr = [];
-
-  while (movements.length > 0) {
-    arr.push(movements.pop());
-  }
-  console.log(arr.join('; '));
 }
 
 function heuristic(node, goal, currentCost) {
@@ -111,55 +79,28 @@ function astar(init, goal) {
 
   while (heap.size() > 0) {
 
-    // console.log('--Heap BEFORE Pop --');
-    // console.log(heap);
     node = heap.pop();
-    // console.log('--Heap After Pop --');
-    // console.log(heap);
-    // node.print();
 
     if (state.compare(node.state, goal)) {
       cost = node.cost;
       while (node.parent) {
-        // node.print();
-        movements.push(node.action);
+        movements.push(node);
         node = node.parent;
       }
       return true;
     }
 
-    visited.push(node.state);
+    visitedNodes.push(node.state);
 
     var children = crane.nextValidStates(node.state);
-    // console.log('Posibilities:', children);
 
     for (var i = 0; i < children.length; i++) {
       child = children[i];
-      if (!visitedContains(child)) {
+      if (!state.visitedContains(visitedNodes, child)) {
         var currentCost = node.cost;
         var newNode = new Node(child, node);
         heap.push(newNode);
-        // console.log('--Heap after push --');
-        // console.log(heap);
       }
     }
   }
-}
-
-function visitedContains(stack) {
-
-  result = false;
-
-  for (j = 0; j < visited.length; j++) {
-    // console.log('--VisitedContains--');
-    // console.log('Checking:', stack);
-    // console.log('Against', visited[j]);
-    // console.log('Result:', state.compare(visited[j], stack));
-
-    if (state.compare(visited[j], stack)) {
-      result = true;
-    }
-  }
-
-  return result;
 }
